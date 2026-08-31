71d48b4a49c3b6beacca03b28aa48334be0f6b32
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-08-31 11:30:50 -0300

    agrega sonido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import HomeCanvas from "../components/Home/HomeCanvas";
 
 export default function Home() {
+  const [audioEnabled, setAudioEnabled] = useState(true);
+
   return (
     <>
       <title>rnz0_</title>
@@ -11,17 +14,26 @@ export default function Home() {
         content="Lab de experimentación digital"
       />
       <div className="relative h-svh w-svw">
-        <div className="p-4 absolute w-full h-full flex items-end justify-end">
-          <div className="w-full">
-            <div className="flex items-center justify-between">
-              <div className="flex-1"></div>
-              {/* <p className="text-xs tracking-wider">
-                laboratorio de experimentación digital.
-              </p> */}
-            </div>
-          </div>
-        </div>
-        <HomeCanvas />
+        {/* <div className="absolute bottom-15 right-5">
+          <button
+            onClick={() => setAudioEnabled((prev) => !prev)}
+            className={`
+                  pointer-events-auto bg-white
+                  font-mono text-[10px] tracking-[0.2em] uppercase
+                  border w-15 h-15 transition-all duration-300 rounded-full cursor-pointer
+                  ${
+                    audioEnabled
+                      ? "border-neutral-300 text-neutral-300"
+                      : "border-neutral-700 text-neutral-700 hover:border-neutral-500 hover:text-neutral-500"
+                  }
+                `}
+          >
+            <span className="mr-2 inline-block">
+              {audioEnabled ? "◉" : "○"}
+            </span>
+          </button>
+        </div> */}
+        <HomeCanvas audioEnabled={audioEnabled} />
       </div>
     </>
   );
